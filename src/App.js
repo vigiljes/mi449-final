216beea0f1cbaef16f1b8fef6301003e4b40b28d
vigiljes <vigiljes@msu.edu>
2023-05-04 12:32:28 -0400

    gawd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React yes no maybe so
         </a>
       </header>
     </div>
