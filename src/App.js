0fa2dbc530df43f28d2619119c2003f832ddb479
vigiljes <vigiljes@msu.edu>
2023-05-04 12:07:52 -0400

    pleaseGawd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> lemmethsink about it.
+          Edit <code>src/App.js</code> lemmethsink abodut it.
         </p>
         <a
           className="App-link"
