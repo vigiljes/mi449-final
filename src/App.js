4e9f6f018ec87b1deab80882330beefb5e990efe
vigiljes <vigiljes@msu.edu>
2023-05-04 12:19:24 -0400

    wowowowowowowowowowow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> lemmethsink abodut,,. it.
+          Edit <code>src/App.js</code> lemmethsink ut,,. it.
         </p>
         <a
           className="App-link"
